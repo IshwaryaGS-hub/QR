025cb99bea343a693c7947dcd4c00a7d488044ab
IshwaryaGS-hub <ishwaryagopinathan@gmail.com>
2024-01-02 11:56:35 +0530

    commit to new files added

---
+++ home.js
@@ -0,0 +1,11 @@
+document.getElementById('someText').innerHTML ="Hello World"
+var age = prompt("What is your age?");
+document.getElementById("demo").innerHTML= age;
+
+var num1 = 10;
+num1++; //increment;
+num1--; //decrement;
+console.log(num1);
+
+
+
